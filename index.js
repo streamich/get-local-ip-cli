327996fa9af8250edb464f52f43db52b63fcd9d9
streamich <streamich@gmail.com>
2019-11-17 23:20:52 +0100

    feat: 🎸 add initial implementation

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const os = require('os');
+
+const ifaces = os.networkInterfaces();
+
+for (const name in ifaces) {
+    const iface = ifaces[name];
+    for (let i = 0; i < iface.length; i++) {
+        const details = iface[i];
+        if (details.family === 'IPv4' && details.internal === false) {
+            process.stdout.write(details.address);
+            process.exit(0);
+        }
+    }
+}
+
+process.stdout.write('127.0.0.1');
